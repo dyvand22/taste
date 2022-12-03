b59cd600a291eb53f4f4d7df5b60b2de29a5c347
Divya Anand <priyanshu9708saps@gmail.com>
2022-12-04 00:06:07 +0530

    file commited

---
+++ 01_practice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int length, breadth;
+    printf("What is the length of the rectangle\n");
+    scanf("%d", &length);
+
+    printf("What is the Breadth of the rectangle\n");
+
+    scanf("%d", &breadth);
+
+    printf("The area Of Your rectangle is %d", length*breadth);
+    return 0;
+
+}
---
+++ first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int b = 8.8;
+    scanf("%d", &b);
+    
+    int c = 9.9;
+    scanf("%d", &c);
+   
+    int d = b + c;
+      scanf("%d", &d);
+
+
+    printf("the value of b is %d \n", b);
+    printf("The vallue of c is %d \n", c);
+    printf("the sum of the value of b and c is %d\n", d);
+   
+    return 0;
+}
